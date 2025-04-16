7360e7b17ecc8302599cd932f83e74ccb3e5db2d
Sanchit agrawal <125626091+sanxxit@users.noreply.github.com>
2025-04-17 00:11:12 +0530

    del method

--- rewrite_commits.py
+++
@@ -1,6 +0,0 @@
-
-def rewrite_commit_callback(commit, metadata):
-    commit.author_name = b"Sanchit Agrawal"
-    commit.author_email = b"sanchitagrawal2023@gmail.com"
-    commit.committer_name = b"Sanchit Agrawal"
-    commit.committer_email = b"sanchitagrawal2023@gmail.com"
